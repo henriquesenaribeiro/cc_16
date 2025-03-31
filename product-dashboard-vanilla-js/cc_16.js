584dc64732ed2c3411227bd0c787739c48a23f93
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-03-30 23:38:40 -0300

    Task 2: Fetch using .then()

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,1 +1,18 @@
-// Part 1
+// Task 2: Fetch using .then()
+function fetchProductsThen() {
+    fetch('https://www.course-api.com/javascript-store-products')
+      .then(response => response.json()) 
+      // Add the JSON
+      .then(products => {
+        // Log each product name to the console
+        products.forEach(product => {
+          console.log(product.fields.name);
+        });
+      })
+      .catch(error => {
+        // Log if there's any error during the fetch
+        console.error("Error with .then() fetch:", error);
+      });
+  }
+// Call the function to fetch products using .then()
+
